93e52268f4d4e9ea3f71cd804cb338d4767874be
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-12-22 00:33:11 -0300

    Inserindo dados na tabela (insert into)

---
+++ aula_5.sql
@@ -0,0 +1,25 @@
+insert into pessoas
+(id, nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+(default, 'Godofredo', '1984-02-01', 'M', '78.5', '1.83', 'Brasil');
+
+-- insert into pessoas
+-- (nome, nascimento, sexo, peso, altura, nacionalidade)
+-- values
+-- ('Maria', '1999-12-30', 'F', '55.5', '1.65', 'Portugal');
+
+-- insert into pessoas
+-- (id, nome, nascimento, sexo, peso, altura, nacionalidade)
+-- values
+-- (Default, 'Creuza', '1920-12-30', 'F', '50.5', '1.65', default);
+
+
+-- insert into pessoas values
+-- (default, 'Adalgiza', '1930-11-2', 'F', '63.2', '1.75', 'Irlanda')
+
+insert into pessoas values
+(default, 'Cláudio', '1975-4-22', 'M', '99.0', '2.15', default),
+(default, 'Pedro', '1999-12-3', 'M', '77', '2', default),
+(default, 'Janaína', '1987-11-12', 'F', '75.4', '1.66', 'EUA');
+
+select * from pessoas;
