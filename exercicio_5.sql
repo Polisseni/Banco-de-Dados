8e2ceb58628c1fe8f0d5fea61d889bf28af5e563
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-01-14 23:57:54 -0300

    ESTACIONAMENTO: exemplo com INSERT

---
+++ exercicio_5.sql
@@ -0,0 +1,6 @@
+-- ESTACIONAMENTO
+
+-- Exemplo INSERT
+
+INSERT INTO estacionamento (veiculo, modelo, placa, data_entrada, horario_entrada)
+VALUES ('Carro', 'Civic', 'ABC1D23', '2026-01-14', '08:30:00');
