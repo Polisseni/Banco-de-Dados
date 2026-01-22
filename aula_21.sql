5347eb92d7558605203550ef90a4ae96190a815c
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-01-22 18:55:01 -0300

    Comando JOIN (parte 2) : criando a tabela gafanhoto_assiste_curso

---
+++ aula_21.sql
@@ -0,0 +1,16 @@
+-- JOIN COM VARIAS TABELAS
+
+create table gafanhoto_assiste_curso (
+	id int not null auto_increment, 
+    data date,
+    idgafanhoto int,
+    idcurso int,
+    primary key (id),
+    foreign key (idgafanhoto) references gafanhotos (id),
+    foreign key (idcurso) references cursos (idcurso)
+) default charset utf8;
+
+insert into gafanhoto_assiste_curso values
+(default, '2014-03-01', '1', '2');
+
+select * from gafanhoto_assiste_curso;
